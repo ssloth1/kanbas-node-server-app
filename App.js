306aef1c0ee718629d1c8c84d2346ca7cfa0e15b
ssloth1 <jamesbebarski@gmail.com>
2024-08-04 02:24:42 -0400

    Modules implemented in node backend

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Hello from "./Hello.js"
 import Lab5 from './Lab5/index.js'
 
 import CourseRoutes from './Kanbas/Courses/routes.js'
+import ModuleRoutes from './Kanbas/Modules/routes.js'
 
 const app = express();
 app.use(cors());
@@ -15,6 +16,7 @@ app.use(express.json()); // Work should start below this line
 Lab5(app);
 Hello(app);
 CourseRoutes(app);
+ModuleRoutes(app);
 
 
 app.listen(process.env.PORT || 4000, () => {

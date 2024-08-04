ee9c1eca4080b1e5ae5cafba6585d72016b64a8c
ssloth1 <jamesbebarski@gmail.com>
2024-08-04 01:21:53 -0400

    lab 5 work

--- Lab5/WorkingWithArrays.js
+++ Lab5/WorkingWithArrays.js
@@ -74,4 +74,29 @@ export default function WorkingWithArrays(app) {
 		res.json(todos);
 	});
 
+	app.post("/lab5/todos", (req, res) => {
+		const newTodo = { ...req.body, id: new Date().getTime() };
+		todos.push(newTodo);
+		res.json(newTodo);
+	});
+
+	app.delete("/lab5/todos/:id", (req, res) => {
+		const { id } = req.params;
+		const todoIndex = todos.findIndex((t) => t.id === parseInt(id));
+		todos.splice(todoIndex, 1);
+		res.sendStatus(200);
+	});
+
+	app.put("/lab5/todos/:id", (req, res) => {
+		const { id } = req.params;
+		todos = todos.map((t) => {
+			if (t.id === parseInt(id)) {
+				return { ...t, ...req.body };
+			}
+			return t;
+		});
+		res.sendStatus(200);
+	});
+
+
 };

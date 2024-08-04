90a63c898bb1d9da2aa6a1e766727e8855910360
ssloth1 <jamesbebarski@gmail.com>
2024-08-04 14:52:40 -0400

    assignment routes added

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import Lab5 from './Lab5/index.js'
 
 import CourseRoutes from './Kanbas/Courses/routes.js'
 import ModuleRoutes from './Kanbas/Modules/routes.js'
+import AssignmentRoutes from './Kanbas/Assignments/routes.js'
 
 const app = express();
 app.use(cors());
@@ -17,6 +18,7 @@ Lab5(app);
 Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
+AssignmentRoutes(app);
 
 
 app.listen(process.env.PORT || 4000, () => {
---
+++ Kanbas/Assignments/routes.js
@@ -0,0 +1,45 @@
+// kanbas-node-server/Kanbas/Assignments/routes.js
+import db from "../Database/index.js";
+
+export default function AssignmentRoutes(app) {
+	// Retrieve assignments for a specific course
+	app.get("/api/courses/:cid/assignments", (req, res) => {
+		const { cid } = req.params;
+		const assignments = db.assignments.filter((a) => a.course === cid);
+		res.json(assignments);
+	});
+
+	// Create a new assignment for a specific course
+	app.post("/api/courses/:cid/assignments", (req, res) => {
+		const { cid } = req.params;
+		const newAssignment = {
+			...req.body,
+			course: cid,
+			_id: new Date().getTime().toString(),
+		};
+		db.assignments.push(newAssignment);
+		res.send(newAssignment);
+	});
+
+	// Update an assignment by ID
+	app.put("/api/assignments/:aid", (req, res) => {
+		const { aid } = req.params;
+		const assignmentIndex = db.assignments.findIndex((a) => a._id === aid);
+		if (assignmentIndex !== -1) {
+			db.assignments[assignmentIndex] = {
+				...db.assignments[assignmentIndex],
+				...req.body,
+			};
+			res.sendStatus(204);
+		} else {
+			res.sendStatus(404);
+		}
+	});
+
+	// Delete an assignment by ID
+	app.delete("/api/assignments/:aid", (req, res) => {
+		const { aid } = req.params;
+		db.assignments = db.assignments.filter((a) => a._id !== aid);
+		res.sendStatus(200);
+	});
+}

3f971b7feed80cad7834de70632666c9840b3337
ssloth1 <jamesbebarski@gmail.com>
2024-08-06 02:06:26 -0400

    work

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
+import "dotenv/config";
 import express from 'express'
 import cors from 'cors'
+import mongoose from 'mongoose'
+
+
+import UserRoutes from './Kanbas/Users/routes.js'
 
 import Hello from "./Hello.js"
 import Lab5 from './Lab5/index.js'
-
 import CourseRoutes from './Kanbas/Courses/routes.js'
 import ModuleRoutes from './Kanbas/Modules/routes.js'
 import AssignmentRoutes from './Kanbas/Assignments/routes.js'
@@ -12,6 +16,8 @@ const app = express();
 app.use(cors());
 app.use(express.json()); // Work should start below this line
 
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 
 
 Lab5(app);
@@ -19,6 +25,7 @@ Hello(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
+UserRoutes(app);
 
 
 app.listen(process.env.PORT || 4000, () => {
